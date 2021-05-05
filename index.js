b516e74b3cfda2723847fb2c58ddf61c08c713c1
SergioFC1989 <sergiofc@hotmail.es>
2021-05-05 04:34:14 +0200

    Finish endpoint

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const {
   PAYCOMET_SEPA_PROVIDER_ID, 
 } = process.env
 
-const BASE_URL = 'https://rest.paycomet.com'
+const BASE_URL = 'https://rest.paycomet.com/v1'
 const HEADERS = {'PAYCOMET-API-TOKEN': PAYCOMET_API_KEY,}
 
-export const addDocument = (merchantCustomerId, merchantCustomerIban, documentType, fileContent) => axios.post(
-  `${BASE_URL}/v1/sepa/add-document`,
+// Required: documentType,merchantCode,merchantCustomerIban,merchantCustomerId,sepaProviderId,terminal
+export const addDocument = (
+merchantCustomerId, 
+merchantCustomerIban, 
+documentType, 
+fileContent
+) => axios.post(
+  `${BASE_URL}/sepa/add-document`,
   { 
     headers: HEADERS,
     body: {
@@ -26,8 +32,13 @@ export const addDocument = (merchantCustomerId, merchantCustomerIban, documentTy
   }
 );
 
-export const checkCustomer = (merchantCustomerId, merchantCustomerIban, merchantCustomerType) => axios.post(
-  `${BASE_URL}/v1/sepa/check-customer`,
+// Required: merchantCode,merchantCustomerIban,merchantCustomerId,merchantCustomerType,sepaProviderId,terminal
+export const checkCustomer = (
+merchantCustomerId, 
+merchantCustomerIban, 
+merchantCustomerType
+) => axios.post(
+  `${BASE_URL}/sepa/check-customer`,
   { 
     headers: HEADERS,
     body: {
@@ -41,8 +52,13 @@ export const checkCustomer = (merchantCustomerId, merchantCustomerIban, merchant
   }
 );
 
-export const checkDocument = (merchantCustomerId, merchantCustomerIban, documentType) => axios.post(
-  `${BASE_URL}/v1/sepa/check-document`,
+// Required: documentType,merchantCode,merchantCustomerIban,merchantCustomerId,sepaProviderId,terminal
+export const checkDocument = (
+merchantCustomerId, 
+merchantCustomerIban, 
+documentType
+) => axios.post(
+  `${BASE_URL}/sepa/check-document`,
   { 
     headers: HEADERS,
     body: {
@@ -52,6 +68,69 @@ export const checkDocument = (merchantCustomerId, merchantCustomerIban, document
       merchantCustomerId,
       merchantCustomerIban,
       documentType,
+    }
+  }
+);
+
+// Required: operations,sepaProviderId,terminal
+export const sepaOperations = (
+operationType,
+terminalIDDebtor,
+uniqueIdCreditor,
+companyNameCreditor,
+ibanNumberCreditor,
+swiftCodeCreditor,
+companyTypeCreditor,
+operationOrder,
+operationAmount,
+operationCurrency,
+operationDatetime,
+operationConcept
+) => axios.post(
+  `${BASE_URL}/sepa/operations`,
+  { 
+    headers: HEADERS,
+    body: {
+      terminal: PAYCOMET_PROVIDER_ID,
+      sepaProviderId: PAYCOMET_SEPA_PROVIDER_ID,
+      operations: [
+        {
+          merchantCode: PAYCOMET_MERCHANT_CODE,
+          operationType,
+          terminalIDDebtor,
+          uniqueIdCreditor,
+          companyNameCreditor,
+          ibanNumberCreditor,
+          swiftCodeCreditor,
+          companyTypeCreditor,
+          operationOrder,
+          operationAmount,
+          operationCurrency,
+          operationDatetime,
+          operationConcept,
+        }
+      ]
+    }
+  }
+);
+
+// Required: operationType
+export const form = (order, amount,) => axios.post(
+  `${BASE_URL}/form`,
+  { 
+    headers: HEADERS,
+    body: {
+      operationType: 1, 
+      language: "es",
+      payment: {
+        terminal: PAYCOMET_PROVIDER_ID,
+        order,
+        amount,
+        methods: [1],
+        currency: "EUR",
+        userInteraction: 1,
+        secure: 1
+      }
     }
   }
 );
